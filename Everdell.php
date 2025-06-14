1b6d68fdda8f9566ae9cdcd2e80931a855e319e6
hadesman19 <hadyn_lee@yahoo.com>
2025-06-14 05:20:52 -0500

    Add files via upload

---
+++ Everdell.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Everdell</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <h1>Everdell</h1>
+    <h2>Scores</h2>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,10 @@
+function loadEverdellPage() {
+    window.open("Everdell.php","_self");
+}
+function loadPickleballPage() {
+    window.open("Pickleball.php","_self");
+}
+function loadMarioPartyPage() {
+    window.open("Mario-Party.php","_self");
+}
+
